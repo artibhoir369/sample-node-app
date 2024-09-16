8fac5d04c4544d0eb21944527b7f64da47bc9ca4
sai <sai@example.com>
2024-09-16 23:19:05 +0530

    Updateded Jenkinsfile

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+	res.send('<h1>Welcome to My Resume Server!</h1><p><a href="/resume.pdf">Download my resume</a></p>');
 });
 
 app.listen(port, () => {
